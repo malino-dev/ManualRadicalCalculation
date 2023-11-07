d4a6019f39534c4095fb5dc61be81e9a3e01ddde
Marino Zirks <marino.zirks@gmail.com>
2023-11-07 14:01:11 +0100

    alod refactoring

--- ManualRadicalCalculation/Program.cs
+++ ManualRadicalCalculation/Program.cs
@@ -1,143 +1,55 @@
-﻿namespace ManualRadicalCalculation;
+﻿using System.Runtime.InteropServices.Marshalling;
+
+namespace ManualRadicalCalculation;
 
 internal class Program
 {
+    static string input = null!;
+    static int extraZeroes = 100;
+    static double factor = 1.0;
+
     static void Main(string[] args)
     {
-        Console.Write("Input:     ");
+        ReadInputs();
+        PreProcess();
 
-        string input = null!;
-        //input = "625";
-        if (input == null) input = Console.ReadLine() ?? throw new ArgumentNullException(nameof(input));
-        else Console.WriteLine("{0}", input);
+        RadicalFinder finder = new();
+        double result = finder.Process(input);
+        Console.WriteLine("Output:    {0}", result / factor);
+        Console.WriteLine("Actual:    {0}", Math.Sqrt(double.Parse(input)) / factor);
+        Console.WriteLine();
+
+        Main(args);
+    }
+
+    private static void ReadInputs()
+    {
+        Console.Write("Input:     ");
+        input = Console.ReadLine() ?? throw new ArgumentNullException(nameof(input));
 
         Console.Write("Extra 0's: ");
-        int extraZeroes = -1;
-        extraZeroes = 10;
-        if(extraZeroes < 0) extraZeroes = int.Parse(Console.ReadLine() ?? throw new ArgumentNullException(nameof(extraZeroes)));
-        else Console.WriteLine("{0}", extraZeroes);
+        //extraZeroes = int.Parse(Console.ReadLine() ?? throw new ArgumentNullException(nameof(extraZeroes)));
+        Console.WriteLine();
+    }
 
+    private static void PreProcess()
+    {
         if (extraZeroes > 0)
         {
             if (!input.Contains('.')) input += '.';
             input += new string('0', Ceiling(extraZeroes, 2));
         }
 
-        double factor = 1;
+        factor = 1;
 
-        if(input.Contains('.'))
+        if (input.Contains('.'))
         {
             int index = input.IndexOf('.');
-            string sub = input.Substring( index + 1);
+            string sub = input.Substring(index + 1);
             double rounded = Round(sub.Length, 2);
-            factor = (double)Math.Pow(10, rounded / 2);
+            factor = Math.Pow(10, rounded / 2);
             input = input.Remove(index, 1);
         }
-
-        string[] groups = MakeGroups(input);
-        int groupIndex = 0;
-
-        double ergebnis, divisor, ans, group, firstSquare, division, subtrahend;
-        string groupStr;
-
-        ergebnis = 0;
-
-        groupStr = groups[groupIndex];
-        group = int.Parse(groupStr);
-        firstSquare = GetClosestStartingRadical((double)group);
-        ergebnis = ergebnis * 10 + (int)Math.Sqrt((double)firstSquare);
-        ans = group - firstSquare;
-        groupIndex++;
-
-        while (groupIndex < groups.Length)
-        {
-            groupStr = groups[groupIndex];
-            group = int.Parse(groupStr[0].ToString());
-            ans = ans * 10 + group;
-            divisor = ergebnis * 2;
-            
-            division = (int)(ans / divisor);
-            if (division > 9) division = 9;
-            
-            while ((divisor * 10 + division) * division > (ans * 10 + int.Parse(groupStr[1].ToString())))
-            {
-                division--;
-            }
-
-            ergebnis = ergebnis * 10 + division;
-
-            subtrahend = (divisor * 10 + division) * division;
-            ans = ans * 10 + int.Parse(groupStr[1].ToString());
-            ans = ans - subtrahend;
-            groupIndex++;
-        }
-
-        ergebnis /= factor;
-
-        Console.WriteLine("Result:    {0}", ergebnis);
-        Console.WriteLine("Actual:    {0}", (double)Math.Sqrt(double.Parse(input)) / factor);
-        Console.WriteLine();
-
-        Main(args);
-    }
-
-    private static string[] MakeGroups(string input)
-    {
-        int i = 0;
-        string[] inputSplit = input.Split('.');
-        string inputInteger = inputSplit[0];
-        string inputdouble = "";
-        
-        if (inputSplit.Length > 1)
-        {
-            inputdouble = inputSplit[1];
-
-            if(inputdouble.Length % 2 == 1)
-            {
-                inputdouble += "0";
-            }
-        }
-
-        List<string> groups = new();
-        
-        if (inputInteger.Length % 2 == 1)
-        {
-            i = 1;
-            groups.Add(inputInteger[0].ToString());
-        }
-
-        for(; i < inputInteger.Length; i += 2)
-        {
-            groups.Add(inputInteger[i].ToString() + inputInteger[i + 1].ToString());
-        }
-
-        if(inputdouble.Length > 0)
-        {
-            groups.Add(".");
-        }
-
-        for(i = 0; i < inputdouble.Length; i += 2)
-        {
-            groups.Add(inputdouble[i].ToString() + inputdouble[i + 1].ToString());
-        }
-
-        return groups.ToArray();
-    }
-
-    private static int GetClosestStartingRadical(double group)
-    {
-        return group switch
-        {
-            < 4 => 1,
-            < 9 => 4,
-            < 16 => 9,
-            < 25 => 16,
-            < 36 => 25,
-            < 49 => 36,
-            < 64 => 49,
-            < 81 => 64,
-            >= 81 => 81
-        };
     }
 
     private static int Round(double value, double step)
@@ -149,4 +61,6 @@ internal class Program
     {
         return (int)(Math.Ceiling(value / step) * step);
     }
+
+
 }
---
+++ ManualRadicalCalculation/RadicalFinder.cs
@@ -0,0 +1,137 @@
+﻿using System.Numerics;
+
+namespace ManualRadicalCalculation;
+
+public class RadicalFinder
+{
+    private string[] groups = null!;
+    private int groupIndex, firstSquare;
+    private double ergebnis, divisor, ans, group, division, subtrahend;
+    private string groupStr = null!;
+
+    public double Process(string input)
+    {
+        groups = MakeGroups(input);
+        groupIndex = 0;
+
+        ProcessFirstRadical();
+
+        while (groupIndex < groups.Length)
+        {
+            ProcessNextGroup();
+        }
+
+        return ergebnis;
+    }
+
+    private void ProcessFirstRadical()
+    {
+        ergebnis = 0;
+        groupStr = groups[groupIndex];
+        group = int.Parse(groupStr);
+        firstSquare = GetClosestStartingRadical(group);
+        ergebnis = ergebnis * 10 + GetPerfectSqrt(firstSquare);
+        ans = group - firstSquare;
+        groupIndex++;
+    }
+
+    private void ProcessNextGroup()
+    {
+        groupStr = groups[groupIndex];
+        group = int.Parse(groupStr[0].ToString());
+        ans = ans * 10 + group;
+        divisor = ergebnis * 2;
+
+        division = (int)(ans / divisor);
+        if (division > 9) division = 9;
+
+        while ((divisor * 10 + division) * division > (ans * 10 + int.Parse(groupStr[1].ToString())))
+        {
+            division--;
+        }
+
+        ergebnis = ergebnis * 10 + division;
+
+        subtrahend = (divisor * 10 + division) * division;
+        ans = ans * 10 + int.Parse(groupStr[1].ToString());
+        ans = ans - subtrahend;
+        groupIndex++;
+    }
+
+    private string[] MakeGroups(string input)
+    {
+        int i = 0;
+        string[] inputSplit = input.Split('.');
+        string inputInteger = inputSplit[0];
+        string inputdouble = "";
+
+        if (inputSplit.Length > 1)
+        {
+            inputdouble = inputSplit[1];
+
+            if (inputdouble.Length % 2 == 1)
+            {
+                inputdouble += "0";
+            }
+        }
+
+        List<string> groups = new();
+
+        if (inputInteger.Length % 2 == 1)
+        {
+            i = 1;
+            groups.Add(inputInteger[0].ToString());
+        }
+
+        for (; i < inputInteger.Length; i += 2)
+        {
+            groups.Add(inputInteger[i].ToString() + inputInteger[i + 1].ToString());
+        }
+
+        if (inputdouble.Length > 0)
+        {
+            groups.Add(".");
+        }
+
+        for (i = 0; i < inputdouble.Length; i += 2)
+        {
+            groups.Add(inputdouble[i].ToString() + inputdouble[i + 1].ToString());
+        }
+
+        return groups.ToArray();
+    }
+
+    private int GetClosestStartingRadical(double group)
+    {
+        return group switch
+        {
+            < 4 => 1,
+            < 9 => 4,
+            < 16 => 9,
+            < 25 => 16,
+            < 36 => 25,
+            < 49 => 36,
+            < 64 => 49,
+            < 81 => 64,
+            >= 81 => 81,
+            _ => throw new NotImplementedException("this case should not occur")
+        };
+    }
+
+    private int GetPerfectSqrt(int square)
+    {
+        return square switch
+        {
+            1 => 1,
+            4 => 2,
+            9 => 3,
+            16 => 4,
+            25 => 5,
+            36 => 6,
+            49 => 7,
+            64 => 8,
+            81 => 9,
+            _ => throw new NotImplementedException("this case should not occur")
+        };
+    }
+}
